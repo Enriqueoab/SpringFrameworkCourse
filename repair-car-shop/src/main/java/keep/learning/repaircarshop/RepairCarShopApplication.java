387ff809aac48e417544d7867b233c3f5d906e5a
Enrique Barca <barcaenrique7@gmail.com>
2021-11-27 20:33:55 +0000

    Starting new project

---
+++ repair-car-shop/src/main/java/keep/learning/repaircarshop/RepairCarShopApplication.java
@@ -0,0 +1,13 @@
+package keep.learning.repaircarshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairCarShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairCarShopApplication.class, args);
+    }
+
+}
---
+++ repair-car-shop/src/test/java/keep/learning/repaircarshop/RepairCarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package keep.learning.repaircarshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairCarShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
