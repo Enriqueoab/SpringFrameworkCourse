949c0160bf6d4a5996b08d25f9286e6f7b02d20d
Enrique Barca <barcaenrique7@gmail.com>
2021-12-05 08:42:52 +0000

    Starting DP examples project

---
+++ dependency-injection-practice/dependency-injection-practice/src/main/java/keep/learning/dependencyinjectionpractice/DependencyInjectionPracticeApplication.java
@@ -0,0 +1,13 @@
+package keep.learning.dependencyinjectionpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionPracticeApplication.class, args);
+	}
+
+}
---
+++ dependency-injection-practice/dependency-injection-practice/src/test/java/keep/learning/dependencyinjectionpractice/DependencyInjectionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package keep.learning.dependencyinjectionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
