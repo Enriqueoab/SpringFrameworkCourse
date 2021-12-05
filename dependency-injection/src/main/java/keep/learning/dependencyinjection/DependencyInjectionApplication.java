847ec5790a1dace3ad38c6330c46d8400ac04d1c
Enrique Barca <barcaenrique7@gmail.com>
2021-12-05 21:08:54 +0000

    New practice

---
+++ dependency-injection/src/main/java/keep/learning/dependencyinjection/DependencyInjectionApplication.java
@@ -0,0 +1,23 @@
+package keep.learning.dependencyinjection;
+
+import keep.learning.dependencyinjection.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = SpringApplication.run(DependencyInjectionApplication.class, args);
+		//The spring framework is managing the construction of myController
+		//We don't have to create a new object
+		MyController controller = (MyController) ac.getBean("myController");
+
+		String hi = controller.sayHellow();
+		System.out.println(hi);
+	}
+
+
+
+}
---
+++ dependency-injection/src/main/java/keep/learning/dependencyinjection/controllers/MyController.java
@@ -0,0 +1,14 @@
+package keep.learning.dependencyinjection.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHellow(){
+
+        System.out.println("Hello world!!");
+        return "Hey, How are you doing?";
+    }
+
+}
