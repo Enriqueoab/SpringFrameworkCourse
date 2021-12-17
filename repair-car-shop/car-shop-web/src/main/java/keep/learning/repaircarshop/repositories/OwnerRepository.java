7c7fae9f62e31b0d7ca046c709cb7e6a122041fd
Enrique Barca <barcaenrique7@gmail.com>
2021-12-17 20:22:00 +0000

    Course practice aplication

--- repair-car-shop/car-shop-web/src/main/java/keep/learning/repaircarshop/repositories/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package keep.learning.repaircarshop.repositories;
-
-import keep.learning.repaircarshop.model.Owner;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OwnerRepository extends CrudRepository<Owner,Long> {
-}
